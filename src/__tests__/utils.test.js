6fe6ac1f6b83a87566699c0415116359e6c736dc
Brian Cheruiyot <briancheru11@gmail.com>
2025-07-27 19:28:50 +0300

    complete lab

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,14 @@
 // Your tests here
+import { pointsForWord } from "../utils";
+
+describe("pointsForWord", () => {
+  it("calculates the total points for a word (1 point per vowel, 2 per consonant)", () => {
+    const word = "test";
+    expect(pointsForWord(word)).toBe(7);
+  });
+
+  it("handles uppercase and lowercase input", () => {
+    const word = "tEsT";
+    expect(pointsForWord(word)).toBe(7);
+  });
+});
